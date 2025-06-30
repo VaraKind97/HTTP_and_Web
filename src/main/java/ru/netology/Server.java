61e004670cf6ed2b2d609a620edac5fc376043a1
VaraKind97 <valadiva@mail.ru>
2025-06-30 18:54:32 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(64);
+        try (ServerSocket server = new ServerSocket(9999)) {
+            System.out.println("Server started!");
+            while (true) {
+                for (int i = 0; i < 100; i++) {
+                    executorService.execute(new Task(i));
+                }
+                executorService.awaitTermination(11, SECONDS);
+
+                executorService.shutdownNow();
+                System.out.println(executorService);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
